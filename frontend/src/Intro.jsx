1fd525cc0746b89c37c5812cb73030574a2454ca
Shredman2000 <shredman2000@gmail.com>
2026-02-08 14:51:00 -0600

    reworked page transition animation

--- frontend/src/Intro.jsx
+++ frontend/src/Intro.jsx
@@ -17,6 +17,10 @@ const obfuscate_symbols = [
   "#", "@"
 ];
 
+
+
+
+
 function ObsfucatingLetter({ letter, index }) {
   const [display, setDisplay] = useState("");
   const intervalRef = useRef(null);
@@ -91,7 +95,7 @@ function Intro() {
   const aboutRef = useRef(null);
   const contactRef = useRef(null);
   const navigate = useNavigate();
-  
+  const [switchingPages, setSwitchingPages] = useState(false);
   const [modalState, setModalState] = useState({
     isOpen: false,
     type: null,
@@ -112,37 +116,45 @@ function Intro() {
       }
     })
   }
+  const pageSwitch = (path) => {
+    setSwitchingPages(true);
 
+    setTimeout(() => {
+      navigate(path);
+    }, 500);
+  }
 
   return (
     <>
-      <div className='intro-page-wrapper intro-page-background'>
-        <header className='intro-page-header'>
-          <h5 className='header-text'>Full Stack Developer</h5>
-        </header>
-        <div className='intro-nav-container'>
-          <h3 ref={aboutRef} onClick={() => openModal('about', aboutRef)} className='intro-nav-text'>About Me</h3>
-          <h3 className='intro-nav-text' onClick={() => navigate('/projects')}>Projects</h3>
-          <h3 className='intro-nav-text' >Contact</h3>
-        </div>
-        <h2 className='hi-im-text'>Hi! I'm...</h2>
-        <div className='large-name-container'>
-          <div className='large-name-wrapper'>
-            {name_letters.map((letter, index) => (
-              <ObsfucatingLetter index={index} letter={letter}/>
-            ))}
+      <div className='intro-page-background'>
+        <div className={`intro-page-wrapper`}>
+          <header className='intro-page-header'>
+            <h5 className='header-text'>Full Stack Developer</h5>
+          </header>
+          <div className={`intro-nav-container ${switchingPages ? "intro-nav-container-leave-page": ""}`}>
+            <h3 ref={aboutRef} onClick={() => openModal('about', aboutRef)} className='intro-nav-text'>About Me</h3>
+            <h3 className='intro-nav-text' onClick={() => pageSwitch('/projects')}>Projects</h3>
+            <h3 className='intro-nav-text' >Contact</h3>
           </div>
-        </div>
-        <div className='interests-text-wrapper'>
-          <div className='interests-text-1'>I'm currently learning and growing as a web developer,</div>
-          <div className='interests-text-2'>focused on turning small projects into learning opportunities. </div>
-        </div>
-        <div className='faceshot-div'>
-          <img src='/Me.png'/>
-        </div>
-        <div className='links-div'>
-          <img src='/linkedinlogo.png'></img>
-          <img src='/githublogo.png'></img>
+          <h2 className={`hi-im-text  ${switchingPages ? "leave-page": ""}`}>Hi! I'm...</h2>
+          <div className={`large-name-container  ${switchingPages ? "leave-page": ""}`}>
+            <div className='large-name-wrapper'>
+              {name_letters.map((letter, index) => (
+                <ObsfucatingLetter key={index} index={index} letter={letter}/>
+              ))}
+            </div>
+          </div>
+          <div className={`interests-text-wrapper ${switchingPages ? "leave-page": ""}`}>
+            <div className='interests-text-1'>I'm currently learning and growing as a web developer,</div>
+            <div className='interests-text-2'>focused on turning small projects into learning opportunities. </div>
+          </div>
+          <div className={`faceshot-div ${switchingPages ? "leave-page": ""}`}>
+            <img src='/Me.png'/>
+          </div>
+          <div className={`links-div ${switchingPages ? "links-div-leave-page": ""}`}>
+            <img src='/linkedinlogo.png'></img>
+            <img src='/githublogo.png'></img>
+          </div>
         </div>
       </div>
       <IntroModal {...modalState} onClose={() => setModalState({isOpen: false, type: null, origin: null })}/>
